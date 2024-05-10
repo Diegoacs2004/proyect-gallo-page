daf4b77990c55efa7e32ab63bf843314c6991d39
Diegoacs2004 <126615170+Diegoacs2004@users.noreply.github.com>
2024-05-10 10:58:51 -0500

    Inicio de la rama develop

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
     <h1>HOLA</h1>
+    input
   </div>
 </template>
 <script lang="ts" setup></script>
-<style scoped></style>
+<style scoped>
+@import "@/scss/styles.scss";
+
+
+</style>
