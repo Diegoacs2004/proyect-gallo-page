e1bad9a740d35efedfc45762a82d0919f2be1dc8
DenisM. <68979805+MyC22@users.noreply.github.com>
2024-05-10 13:10:56 -0500

    formulari registro

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    <h1>HOLA</h1>
-    input
+    <form class="container-registro">
+        <div class="title-init">
+            <h1>INICIAR SESION</h1>
+        </div>
+        <div class="container-inputs">
+            <input type="text" placeholder="Nombre / Correo:">
+            <input type="text" placeholder="Contraseña:">
+        </div>
+            <button>REGISTRARSE</button>
+        <p>¿Olvidaste tu contraseña?</p>
+    </form>
   </div>
 </template>
 <script lang="ts" setup></script>
